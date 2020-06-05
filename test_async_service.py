5dacf827ee7849166d3db063f7222cd78c5ae9fc
vmred <vladdavidchenko@gmail.com>
2020-06-05 21:36:51 +0300

    add async tests

--- test_async_service.py
+++ test_async_service.py
@@ -1,7 +1,22 @@
+import pytest
+
 from async_service import AsyncService
 
+iterations = [x for x in range(1000)]
 
-class TestAsyncService:
-    def test_one(self):
-        async_service = AsyncService()
-        async_service.verify_documents()
+
+@pytest.mark.asyncio_cooperative
+@pytest.mark.parametrize('iteration', iterations, ids=iterations)
+async def test_asyncio_cooperative(iteration):
+    service = AsyncService()
+    # assert service.wait_is_processed(iteration=iteration, timeout=5) == 'https://google.com'
+    await service.is_processed(iteration, timeout=5)
+    assert service.result_url == 'https://google.com'
+
+
+@pytest.mark.parametrize('iteration', iterations, ids=iterations)
+def test_pytest_concurrent(iteration):
+    service = AsyncService()
+    # assert service.wait_is_processed(iteration=iteration, timeout=5) == 'https://google.com'
+    service.wait_is_processed(iteration, timeout=5)
+    assert service.result_url == 'https://google.com'
