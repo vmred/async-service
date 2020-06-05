bbfc75c38c18083eba47d38dee18819d9172bb83
Vlad Davidchenko <Vlad Davidchenko>
2020-06-05 21:33:06 +0300

    add sync and async response wait functions

--- async_service.py
+++ async_service.py
@@ -1,15 +1,40 @@
-import grequests
+import asyncio
+
+import requests
+import time
 
 
 class AsyncService:
     def __init__(self):
         self.base_url = 'https://google.com'
+        self.result_url = None
 
     def get_api_list(self):
         return [self.base_url + '/search?q=first', self.base_url + '/search?q=second',
                 self.base_url + '/search?q=third']
 
-    def verify_documents(self):
-        responses = grequests.map([grequests.get(x) for x in self.get_api_list()])
-        for i in responses:
-            print('--> %s,  %d' % (i.url, i.status_code))
+    def wait_is_processed(self, iteration, timeout, polling=1):
+        i = 0
+        while i < timeout:
+            response = requests.get(f'http://127.0.0.1:5000/get_document?iteration={iteration}').json()
+            if response['status'] == 'success':
+                print(f'---> success, iteration = {iteration}')
+                self.result_url = response['url']
+                break
+
+            print(f'---> pending, iteration = {iteration}')
+            i += polling
+            time.sleep(polling)
+
+    async def is_processed(self, iteration, timeout, polling=1):
+        i = 0
+        while i < timeout:
+            response = requests.get('http://127.0.0.1:5000/get_document').json()
+            if response['status'] == 'success':
+                print(f'---> success, iteration = {iteration}')
+                self.result_url = response['url']
+                break
+
+            print(f'---> pending, iteration = {iteration}')
+            i += polling
+            await asyncio.sleep(polling)
