e10bbac13160f11aaa66969368c96d04fe67142f
vmred <vladdavidchenko@gmail.com>
2020-06-07 19:59:07 +0300

    testing

--- test_async_service.py
+++ test_async_service.py
@@ -2,14 +2,13 @@ import pytest
 
 from async_service import AsyncService
 
-iterations = [x for x in range(1000)]
+iterations = [x for x in range(5)]
 
 
 @pytest.mark.asyncio_cooperative
 @pytest.mark.parametrize('iteration', iterations, ids=iterations)
-async def test_asyncio_cooperative(iteration):
+async def test_async(iteration):
     service = AsyncService()
-    # assert service.wait_is_processed(iteration=iteration, timeout=5) == 'https://google.com'
     await service.is_processed(iteration, timeout=5)
     assert service.result_url == 'https://google.com'
 
@@ -17,6 +16,5 @@ async def test_asyncio_cooperative(iteration):
 @pytest.mark.parametrize('iteration', iterations, ids=iterations)
 def test_pytest_concurrent(iteration):
     service = AsyncService()
-    # assert service.wait_is_processed(iteration=iteration, timeout=5) == 'https://google.com'
     service.wait_is_processed(iteration, timeout=5)
     assert service.result_url == 'https://google.com'
